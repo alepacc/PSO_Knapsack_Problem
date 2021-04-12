213414e84bd209093e7ce42a3fa6e259f9856071
alepacc <alessiopacconi@gmail.com>
2021-04-12 19:59:10 +0200

    pso

--- dataset.py
+++ dataset.py
@@ -1,20 +1,35 @@
 def data(num=0, low=True):
     """
     :param low:(default=True) choose low dimensional instance, False coose large scale dimensional istance
-    :param num: number (1 to 10) of the chosen instance
+    :param num: number (low: 1 to 10, large: 1 to 21) of the chosen instance
     :return: dataset of items fro testing Knapsack PSO
     """
     path = "instances_01_KP"
     low_dim = ['f1_l-d_kp_10_269', 'f2_l-d_kp_20_878', 'f3_l-d_kp_4_20', 'f4_l-d_kp_4_11', 'f5_l-d_kp_15_375',
-               'f6_l-d_kp_10_60', 'f7_l-d_kp_7_50', 'f8_l-d_kp_23_10000', 'f9_l-d_kp_5_80', 'f10_l-d_kp_20_879', "f11_l-d_kp_40_990"]
+               'f6_l-d_kp_10_60', 'f7_l-d_kp_7_50', 'f8_l-d_kp_23_10000', 'f9_l-d_kp_5_80', 'f10_l-d_kp_20_879',
+               "f11_l-d_kp_40_990"]
     large_scale = [
-        "knapPI_1_100_1000_1",
+        "knapPI_1_100_1000_1",  # Uncorrelated data instances
         "knapPI_1_200_1000_1",
         "knapPI_1_500_1000_1",
         "knapPI_1_1000_1000_1",
         "knapPI_1_2000_1000_1",
         "knapPI_1_5000_1000_1",
-        "knapPI_1_10000_1000_1"]
+        "knapPI_1_10000_1000_1",
+        "knapPI_2_100_1000_1",  # Weakly correlated instances
+        "knapPI_2_200_1000_1",
+        "knapPI_2_500_1000_1",
+        "knapPI_2_1000_1000_1",
+        "knapPI_2_2000_1000_1",
+        "knapPI_2_5000_1000_1",
+        "knapPI_2_10000_1000_1",
+        "knapPI_3_100_1000_1",  # Strongly correlated instances
+        "knapPI_3_200_1000_1",
+        "knapPI_3_500_1000_1",
+        "knapPI_3_1000_1000_1",
+        "knapPI_3_2000_1000_1",
+        "knapPI_3_5000_1000_1",
+        "knapPI_3_10000_1000_1"]
     opt_path = "-optimum/"
 
     try:
@@ -55,5 +70,5 @@ def data(num=0, low=True):
         print("Error:", err)
     finally:
         fp.close()
-#        fp_opt.close()
+        fp_opt.close()
     return dataset
--- main.py
+++ main.py
@@ -5,12 +5,11 @@ import dataset as d
 
 
 def sigmoid(n):
-    return 1 / (1 + math.exp(-n))
+    return 1.0 / (1.0 + math.exp(-n))
 
 
 def save_img_plot(file_name):
-    if not (
-            file_name == ''):  # If the variable named 'file_name' is not empty, save the file with that name in png format.
+    if not (file_name == ''):
         file_name = file_name + ".png"
         plt.savefig(file_name)
     # plt.show()
@@ -31,16 +30,17 @@ def profit(items):
         total_profit += items[_] * value[_]  # x * value
     return total_profit
 
+
 # If total weight exceeds max_weight
-def check_weight(items, element):
+def check_weight(items, total_profit):
     total_weight = 0
     for _ in range(len(items)):
         total_weight += items[_] * weight[_]
 
     if total_weight <= max_weight:
-        return element
+        return total_profit
     else:
-        return -element
+        return -total_profit
 
 
 # -------- PARTICLE --------
@@ -53,8 +53,8 @@ class Particle:
         self.position = []  # particle position
         self.velocity = []  # particle velocity
         self.personal_best = []  # best position individual
-        self.err_personal_best = -1  # best error individual
-        self.err_personal = -1  # error individual
+        self.fit_personal_best = -1  # best fitness value
+        self.fit_current = -1  # fitness value of the current solution
 
         self.decrease = decrease
 
@@ -65,19 +65,19 @@ class Particle:
 
     # evaluate current fitness
     def evaluate(self, cost_func):
-        self.err_personal = cost_func(self.position)  # cost_func --> maximize
+        self.fit_current = cost_func(self.position)  # cost_func --> maximize
 
         # check to see if the current position is a personal best
-        if self.err_personal > self.err_personal_best or self.err_personal_best == -1:
+        if self.fit_current > self.fit_personal_best or self.fit_personal_best == -1:
             self.personal_best = self.position
-            self.err_personal_best = self.err_personal
+            self.fit_personal_best = self.fit_current
 
-    def update_velocity(self, global_best, decr):
+    def update_velocity(self, global_best, decr, type_pso):
         global w, c1, c2
         # parameter inertia weight (how much weigh of the previous velocity)
-        if self.decrease:   # LDIW
-            w = round(0.9-decr, 1)
-        else:   # constant w
+        if self.decrease:  # LDIW
+            w = round(0.9 - decr, 1)
+        else:  # constant w
             w = 0.9
         c1 = 1.49  # cognitive constant (particle)
         c2 = 1.49  # social constant (swarm)
@@ -88,31 +88,31 @@ class Particle:
             # compute new velocity of current particle
             cognitive = c1 * r1 * (self.personal_best[i] - self.position[i])
             social = c2 * r2 * (global_best[i] - self.position[i])
-            self.velocity[i] = w * self.velocity[i] + cognitive + social
-        
+            if type_pso == "BPSO":
+                self.velocity[i] = self.velocity[i] + cognitive + social
+            else:
+                self.velocity[i] = w * self.velocity[i] + cognitive + social
 
     def update_position(self, bounds, type_pso):
         for i in range(num_items):
             if type_pso == "BPSO":
-                # _________ BPSO _______#
+                # _________ BPSO _______ #
                 # If velocity is too high, the particle can fly over the best value,
                 # if is too low, the particle can’t make enough exploration. Velocity falls into the local optimum.
-                vel_max = 4   # velocity [-4, 4]
+                vel_max = 4  # velocity [-4, 4]
                 # limit the range of current velocity
                 if self.velocity[i] < -vel_max:
                     self.velocity[i] = -vel_max
                 elif self.velocity[i] > vel_max:
                     self.velocity[i] = vel_max
 
-                # compute new position using new velocity
-                self.position[i] += self.velocity[i]
-
                 if random.uniform(0, 1) < sigmoid(self.velocity[i]):
                     self.position[i] = 1
                 else:
                     self.position[i] = 0
+
             elif type_pso == "PSO":
-                # ### ----- PSO ------
+                # ----- PSO ------ #
                 self.position[i] += self.velocity[i]
                 # check if current position it's out of bound
                 if self.position[i] > bounds[i][1]:
@@ -122,12 +122,13 @@ class Particle:
                     # If the position is below the lower limit value, pull down to the lower limit value
                     self.position[i] = bounds[i][0]
                 else:
-                    self.position[i] = round(self.position[i])  # position of particles: if rand(0,1)<0.5 --> x=0  else x=1
+                    self.position[i] = round(
+                        self.position[i])  # position of particles: if rand(0,1)<0.5 --> x=0  else x=1
 
 
 # -------- PSO --------
 class PSO:
-    fitness, iterations, step_weight, step_profit, global_best, err_global_best = [], [], [], [], [], -1
+    fitness, iterations, step_weight, step_profit, global_best, fit_global_best = [], [], [], [], [], -1
 
     def __init__(self, cost_func, x, bounds, num_particles, max_iter, type_pso, decrease=False, verbose=False):
         """
@@ -144,12 +145,11 @@ class PSO:
         :type verbose: bool
         """
 
-        global num_items  #, linear_decrease
+        global num_items
 
-        # linear_decrease = decrease
-        num_items = len(x)  # numbers of particle
-        self.err_global_best = -1  # best error for group
-        self.global_best = []  # best position for group
+        num_items = len(x)  # number of objects
+        self.fit_global_best = -1  # best fitness value of swarm
+        self.global_best = []  # best position of swarm
 
         # establish the swarm
         swarm = []
@@ -164,9 +164,9 @@ class PSO:
                 swarm[j].evaluate(cost_func)
 
                 # determine if current particle is the best (globally)
-                if swarm[j].err_personal > self.err_global_best or self.err_global_best == -1:
+                if swarm[j].fit_current > self.fit_global_best or self.fit_global_best == -1:
                     self.global_best = list(swarm[j].position)
-                    self.err_global_best = float(swarm[j].err_personal)
+                    self.fit_global_best = float(swarm[j].fit_current)
 
             if decrease:
                 decr = round(0.9 / max_iter * iteration, 1)  # linear decrease inertia w (LDIW) from 0.9 -> 0.0
@@ -175,9 +175,8 @@ class PSO:
 
             # update velocities and position
             for j in range(num_particles):
-                swarm[j].update_velocity(self.global_best, decr)
+                swarm[j].update_velocity(self.global_best, decr, type_pso)
                 swarm[j].update_position(bounds, type_pso)
-
 
             total_profit = 0
             total_weight = 0
@@ -185,9 +184,10 @@ class PSO:
                 total_profit += self.global_best[i] * value[i]
                 total_weight += self.global_best[i] * weight[i]
 
+            # parameters for plotting
             self.step_profit.append(total_profit)
             self.step_weight.append(total_weight)
-            self.fitness.append(self.err_global_best)
+            self.fitness.append(self.fit_global_best)
             self.iterations.append(iteration)
 
             if verbose:
@@ -203,11 +203,12 @@ class PSO:
             total_profit += self.global_best[i] * value[i]
             total_weight += self.global_best[i] * weight[i]
 
-        try:
+        try:  # round float value
             total_profit = round(total_profit, 4)
             total_weight = round(total_weight, 4)
-        except:
+        except ValueError:
             pass
+
 
         print(f'global best -> {self.global_best}')
         print('Profit: ', total_profit, '\nWeight: ', total_weight, '\n', '-' * 20, sep='')
@@ -216,19 +217,19 @@ class PSO:
     def plot_result(self, file_name=''):
         x1, x2, x3, y1, y2, y3 = [], [], [], [], [], []
         for i in range(len(self.iterations)):
-            if i < len(self.iterations) / 3:   # BPSO
+            if i < len(self.iterations) / 3:  # BPSO
                 x1.append(self.step_weight[i])
                 y1.append(self.step_profit[i])
-            elif i < 2*len(self.iterations)/3:  # LDIW-PSO
+            elif i < 2 * len(self.iterations) / 3:  # LDIW-PSO
                 x2.append(self.step_weight[i])
                 y2.append(self.step_profit[i])
-            else:                               # PSO
+            else:  # PSO
                 x3.append(self.step_weight[i])
                 y3.append(self.step_profit[i])
         plt.xlabel('Weight')
         plt.ylabel('Profit')
         plt.suptitle('PSO vs BPSO vs LDIW-PSO ', fontsize=18)
-        plt.title('c1 = '+str(c1)+', c2 = '+str(c2)+', w = '+str(w))
+        plt.title('c1 = ' + str(c1) + ', c2 = ' + str(c2) + ', w = ' + str(w))
         plt.plot(x1, y1, linestyle='--', label="BPSO")
         plt.plot(x2, y2, linestyle='dotted', label="LDIW-PSO")
         plt.plot(x3, y3, linestyle='dashdot', label="PSO")
@@ -240,19 +241,20 @@ class PSO:
         x1, x2, x3, y1, y2, y3 = [], [], [], [], [], []
         for i in range(len(self.iterations)):
             if i < len(self.iterations) / 3:  # BPSO
-                x1.append(self.step_weight[i])
-                y1.append(self.step_profit[i])
-            elif i < 2 * len(self.iterations) / 3:  # LDIW-PSO
-                x2.append(self.step_weight[i])
-                y2.append(self.step_profit[i])
-            else:                               # PSO
-                x3.append(self.step_weight[i])
-                y3.append(self.step_profit[i])
+                x1.append(self.iterations[i])
+                y1.append(self.fitness[i])
+            elif i < 2*len(self.iterations)/3:  # LDIW-PSO
+                x2.append(self.iterations[i])
+                y2.append(self.fitness[i])
+            else:
+                x3.append(self.iterations[i])
+                y3.append(self.fitness[i])
+
         plt.xscale('symlog')
         plt.xlabel('iterations')
         plt.ylabel('fitness')
         plt.suptitle('Evolution of solutions', fontsize=18)
-        plt.title('c1 = '+str(c1)+', c2 = '+str(c2)+', w = '+str(w))
+        plt.title('c1 = ' + str(c1) + ', c2 = ' + str(c2) + ', w = ' + str(w))
         plt.plot(x1, y1, linestyle='--', label="BPSO")
         plt.plot(x2, y2, linestyle='dotted', label="LDIW-PSO")
         plt.plot(x3, y3, linestyle='dashdot', label="PSO")
@@ -264,18 +266,31 @@ class PSO:
 
 # -------- MAIN --------
 def main():
-    global value, weight, max_weight, obj_num, optimum
-    num = input("insert number from 1 to 10: ")
+    global value, weight, max_weight, num_obj, optimum
+
+    # input for choose type of dataset and number of instance
+    try:
+        choice_dim = input("Choose type of Dataset. Low dimensional or Big dim? (l/b): ").lower()
+        if choice_dim == "l":  # Knapsack dataset low dimensional
+            choice_dim = True
+            num = input("Choose one instance for testing. Insert number from 1 to 10: ")
+        elif choice_dim == "b":  # Knapsack dataset large dimensional
+            choice_dim = False
+            num = input("Choose one instance for testing. Insert number from 1 to 21: ")
+        if not isinstance(choice_dim, bool):
+            print("error bool")
+    except ValueError:
+        print("Input error!")
 
     # choose type of dataset (low dimension OR large)
-    dataset = d.data(int(num) - 1, low=True)
+    dataset = d.data(int(num) - 1, low=choice_dim)
     value = []
     weight = []
 
     for i in dataset:
         if i == 0:
             max_weight = dataset[i][1]
-            obj_num = dataset[i][0]
+            num_obj = dataset[i][0]
         elif i != len(dataset) - 1:
             value.append(dataset[i][0])
             weight.append(dataset[i][1])
@@ -285,7 +300,6 @@ def main():
     # -------- EXECUTE --------
     initial = []
     bounds = []
-    length = len(value)
 
     # input for choose type of knapsack
     try:
@@ -302,17 +316,17 @@ def main():
     # generate bounds and vector initial
     if not choice:
         print('\n[lower limit - upper limit]')
-        for i in range(len(weight)):
+        for i in range(num_obj):  # number of objects
             initial.append(0)  # initial position of particle
             bounds.append((initial[i], math.floor(max_weight / weight[i])))
             print('object ', i, ': [', bounds[i][0], '-', bounds[i][1], ']', sep='')
     else:  # Knapsack 0-1
-        upper_bound = [1 for _ in range(length)]  # upper bound
-        for i in range(len(weight)):
+        upper_bound = [1 for _ in range(num_obj)]  # upper bound
+        for i in range(num_obj):
             initial.append(0)
             bounds.append((initial[i], upper_bound[i]))
 
-    print('There are a total of', length, 'variable...\n')
+    print('There are a total of', num_obj, 'objects...\n')
 
     # input max iteration and number of particles
     try:
@@ -323,7 +337,7 @@ def main():
 
 
     # ----- RESULT ------
-    print('-' * 20, '\n# obj: ', obj_num, '\nmax weight: ', max_weight, '\noptimum profit to achieve: ', optimum)
+    print('-' * 20, '\n# obj: ', num_obj, '\nmax weight: ', max_weight, '\noptimum profit to achieve: ', optimum)
     print('-' * 20, '\nFINAL RESULTS:')
 
     pso = PSO(maximize, initial, bounds, num_particles, max_iter, "BPSO")
@@ -331,7 +345,7 @@ def main():
     pso.result()
 
     pso = PSO(maximize, initial, bounds, num_particles, max_iter, "PSO", decrease=True, verbose=False)
-    print('_' * 10, 'LIDW-PSO', '_' * 10, sep='')
+    print('_' * 10, 'LDIW-PSO', '_' * 10, sep='')
     pso.result()
 
     pso = PSO(maximize, initial, bounds, num_particles, max_iter, "PSO")
@@ -340,8 +354,6 @@ def main():
 
     pso.plot_result(file_name="result")
     pso.plot_evolution(file_name="evolution")
-
-
 
 
 if __name__ == '__main__':
